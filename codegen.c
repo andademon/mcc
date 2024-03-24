4485f52d0ca359c8fa381245339df33e98c9a6b9
Anda <ethanhong0530@gmail.com>
2024-03-24 08:42:37 +0800

    change codegen.c: add gen local var

--- codegen.c
+++ codegen.c
@@ -66,8 +66,44 @@ static void gen_gvar(Node *node) {
     }
 }
 
+static int offset = 0;
+
+// 将局部变量保存在内存中，返回记录的偏移地址（Q: 偏移地址如何计算？）
 static void gen_lvar(Node *node) {
-    
+    int len = node->declarators->len;
+    for (int i = 0;i < len;i++) {
+        Node *temp_node = node->declarators->data[i];
+        // 当且仅当变量有赋初值且为常量赋值时
+        if (temp_node->init == NULL 
+            || (temp_node->init->node_type != ND_NUM 
+                && temp_node->init->node_type != ND_CHAR)
+        )
+            continue;
+        // int imm = (int)temp_node->init->value
+        int reg = new_reg();
+        switch(node->decl_type) {
+            case INT: {
+                printf("li r%d, %s\n", reg, temp_node->init->tok->value);
+                printf("sw r%d, -%d(s0)\n", reg, offset);
+                break;
+            }
+            case CHAR: {
+                printf("li r%d, %d\n", reg, (int)temp_node->init->tok->value[0]);
+                printf("sb r%d, -%d(s0)\n", reg, offset);
+                break;
+            }
+        }
+        offset += 4;
+    }
+    // 无初始化，跳过
+    // 有初始化，且初始化类型为以下之一：
+    // INT: NUM -> 加载到寄存器，然后保存到内存，在符号表中记录在内存中的偏移量 -> li，sw
+    // CHAR: CHARACTER, li, sb
+    // BOOL: TRUTH, 同上 li, sb
+}
+
+static int gen_stmt(Node *node){
+
 }
 
 static int gen_binop(Node *node) {
@@ -123,5 +159,6 @@ void emit_code(Node *node) {
 
 void codegen(Node *node) {
     // gen_expr(node);
-    gen_gvar(node);
+    // gen_gvar(node);
+    gen_lvar(node);
 }
