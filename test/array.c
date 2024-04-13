74bdb77a5ab3c465315280ae5a775795d5fe7fb6
Anda <ethanhong0530@gmail.com>
2024-04-14 05:58:54 +0800

    add test case 	modified:   test/array.c 	new file:   test/quicksort.c

--- test/array.c
+++ test/array.c
@@ -1,8 +1,12 @@
-int main() {
-    int a[6] = {10, 5, 7, 1, 3, 2}, i;
-    for (i = 0;i < 6;i++) {
-        printf("%d ", a[i]);
-    }
+void printArray(int arr[], int size) {
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
     printf("\n");
+}
+
+int main() {
+    int a[6] = {10, 5, 7, 1, 3, 2};
+    printArray(a, 6);
     return 0;
 }
